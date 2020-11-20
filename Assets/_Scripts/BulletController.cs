04327099791b8c2e7751bd24e0cbdc02ec3d6527
FlavioJorge AraujoSilva <silvaflavio267@gmail.com>
2020-11-20 12:20:12 -0500

    Lab 8 Finished

--- Assets/_Scripts/BulletController.cs
+++ Assets/_Scripts/BulletController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,17 +8,40 @@ public class BulletController : MonoBehaviour, IApplyDamage
     public float verticalSpeed;
     public float verticalBoundary;
     public int damage;
+    public ContactFilter2D contactFilter2D;
+    public List<Collider2D> colliders;
+    public Vector3 direction;
+
+    private BoxCollider2D boxCollider;
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        direction = Vector3.up;
+    }
     
     // Update is called once per frame
     void Update()
     {
         _Move();
         _CheckBounds();
+        _CheckCollisions();
+    }
+
+    private void _CheckCollisions()
+    {
+        Physics2D.GetContacts(boxCollider, contactFilter2D, colliders);
+
+        if (colliders.Count > 0 && colliders[0] != null)
+        {
+           // Debug.Log("Hit: " + colliders[0].gameObject.name);
+            BulletManager.Instance().ReturnBullet(gameObject);
+        }
+        
     }
 
     private void _Move()
     {
-        transform.position += new Vector3(0.0f, verticalSpeed, 0.0f) * Time.deltaTime;
+        transform.position += direction * verticalSpeed * Time.deltaTime;
     }
 
     private void _CheckBounds()
@@ -28,11 +52,11 @@ public class BulletController : MonoBehaviour, IApplyDamage
         }
     }
 
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        //Debug.Log(other.gameObject.name);
-        BulletManager.Instance().ReturnBullet(gameObject);
-    }
+    //public void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    //Debug.Log(other.gameObject.name);
+    //    BulletManager.Instance().ReturnBullet(gameObject);
+    //}
 
     public int ApplyDamage()
     {
